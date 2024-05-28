967d20e1fb22d4e886c9bad62e702ed0e6568d3d
inmeet <ik822@ic.ac.uk>
2024-05-28 16:53:31 +0100

    Initial setup for firebase and react

---
+++ drp-app/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnUxufFi8ZMxxkK-IXwrIFXaggE96b0zE",
+  authDomain: "drp-47.firebaseapp.com",
+  projectId: "drp-47",
+  storageBucket: "drp-47.appspot.com",
+  messagingSenderId: "57267009849",
+  appId: "1:57267009849:web:e219170cfceca1f2b490e8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
